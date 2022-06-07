31f00ccaa7134a174be0098e1b18189be71cdc40
Gilles <chekunive@gmail.com>
2022-06-08 00:18:25 +0200

    refactorisations

--- src/ncorps3/Calcul3Corps.java
+++ src/ncorps3/Calcul3Corps.java
@@ -1,6 +1,8 @@
 package ncorps3;
 
 import java.math.BigDecimal;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static java.lang.Math.random;
 
@@ -8,23 +10,31 @@ public class Calcul3Corps implements Parametres {
 
 
     public final Corps[][] ncorps;
+    BigDecimal[] d = new BigDecimal[3];
+    Function<Integer, BigDecimal> c = j -> d[j].pow(2, mc);
+    BiFunction<Integer, Corps,BigDecimal> NC = (i,x) -> x.param[i];
+
+    BigDecimal zero = new BigDecimal("0.0");
+    BigDecimal demi = new BigDecimal("0.5");
 
     public Calcul3Corps() {
         CORPS.get().forEach(Calcul3Corps::init);
         ncorps = NCorpsT0;
         T.get().forEach(t -> CORPS.get().forEach(n -> {
-            BigDecimal[] f = new BigDecimal[]{new BigDecimal("0.0"), new BigDecimal("0.0"), new BigDecimal("0.0")};
+            BigDecimal[] f = new BigDecimal[]{zero, zero, zero};
             CORPS.get().filter(m -> n != m).forEach(m -> {
-                BigDecimal[] d = new BigDecimal[3];
-                CORPS.get().forEach(i -> d[i] = ncorps[n][t].param[i].subtract(ncorps[m][t].param[i]));
-                BigDecimal D = d[0].multiply(d[0]).add(d[1].multiply(d[1])).add(d[2].multiply(d[2]));
-                BigDecimal Denom = D.sqrt(mc).pow(3, mc);
+                Corps corpsn = ncorps[n][t];
+                Corps corpsm = ncorps[m][t];
+
+                CORPS.get().forEach(i -> d[i] = corpsn.param[i].subtract(corpsm.param[i]));
+                BigDecimal Denom = c.apply(0).add(c.apply(1)).add(c.apply(2)).sqrt(mc).pow(3, mc);
                 CORPS.get().forEach(i -> f[i] = f[i].add(Gm.multiply(d[i].divide(Denom, scale, rnd))));
 
                 Corps corps = new Corps();
-                CORPS.get().forEach(i -> corps.param[i] = (new BigDecimal("0.5")).multiply(f[i].multiply(pas.pow(2)))
-                        .add((ncorps[n][t].param[i + 3]).multiply(pas)).add(ncorps[n][t].param[i]));
-                CORPS.get().forEach(i -> corps.param[i + 3] = f[i].multiply(pas).add(ncorps[n][t].param[i + 3]));
+
+                CORPS.get().forEach(i -> corps.param[i] = demi.multiply(f[i].multiply(pas.pow(2)))
+                        .add((corpsn.param[i + 3]).multiply(pas)).add(corpsn.param[i]));
+                CORPS.get().forEach(i -> corps.param[i + 3] = f[i].multiply(pas).add(corpsn.param[i + 3]));
 
                 ncorps[n][t + 1] = corps;
             });
